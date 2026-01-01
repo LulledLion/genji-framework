100b5b287a048b6c6d9ea27ba298e5cdd3f910ec
LulledLion:LocalUser <LocalUser@Example.com>
2025-12-31 21:03:39 -0600

    switch macro order

--- mechanics/fwHero.js
+++ mechanics/fwHero.js
@@ -4,6 +4,6 @@ if (Array.isArray(Hero)){
     Result = Hero.map(i => '#!include "mechanics/' + i).join('.opy"\n');
 }
 else{
-    Result ='#!include "mechanics/' + Hero + '.opy"\nmacro FwHero = Hero.' + Hero + '\n';
+    Result ='macro FwHero = Hero.' + Hero + '\n#!include "mechanics/' + Hero + '.opy"\n';
 }
 Hero = Result
