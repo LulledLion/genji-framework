cecdda236e6f7e997b9a5d8fc8932bae46e6ecf8
LulledLion:LocalUser <LocalUser@Example.com>
2026-05-25 22:09:19 -0500

    Fix post compile processing

--- corrections.js
+++ corrections.js
@@ -1,3 +1,16 @@
 //String.prototype.replaceAll = function (search, replace) { return this.split(search).join(replace); };
 //String.prototype.replaceAll = function (search, replace) { return this.replace(new RegExp(search.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"), "g"), replace); };
-content = content.replace(/rule \("Initialize player variables"\) {\n    event {\n        Ongoing - Each Player;\n        All;\n        All;\n    }\n    actions {\n        Set Player Variable\(Event Player, __languageIndex__, 1\.1\);\n    }\n}\n\n?/g, "");
+content = content.replace(`        Set Player Variable(Event Player, __languageIndex__, 0.1);`, "");
+content = content.replace(
+`rule ("OverPy | Global Init") {
+    event {
+        Ongoing - Global;
+    }
+    actions {`,
+`rule ("OverPy | Global Init") {
+    event {
+        Ongoing - Global;
+    }
+    actions {
+        Disable Inspector Recording;`)
+content;
